7b6fb9a00b87ce3a753f20f107d00864cf0f7d6d
Andy Hou <andy.hou1604@gmail.com>
2023-11-27 12:49:56 -0500

    added background

--- src/main/java/edu/vanier/collision/controllers/FXMLAsteroidsLayoutController.java
+++ src/main/java/edu/vanier/collision/controllers/FXMLAsteroidsLayoutController.java
@@ -9,6 +9,11 @@ import edu.vanier.collision.model.Projectile;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 
@@ -27,6 +32,8 @@ public class FXMLAsteroidsLayoutController extends FXMLDefaultAnimationControlle
     @FXML
     public void initialize() {
         super.initialize();
+        animationPane.setBackground(new Background(new BackgroundImage(new Image("/images/starfield_alpha.png"), BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+          BackgroundSize.DEFAULT)));
         btnPlay.setOnAction((event) -> {
             btnRemove.setDisable(false);
             if (circles.isEmpty()) {
@@ -34,8 +41,8 @@ public class FXMLAsteroidsLayoutController extends FXMLDefaultAnimationControlle
             }
             for (int i = 0; i < (int) spObjectCount.getValue(); i++) {
                 // Projectiles will have the same value for mass and radius in order to ensure they're proportional.
-                double random_Mass_Radius = (0.75 + Math.random()) * 10; // All projectiles will have size between 7.5 and 17.5 pixels.
-                Projectile addedCircle = new Projectile(random_Mass_Radius, Math.random() * 10, Math.random() * 10, 20, 40, asteroidImage, random_Mass_Radius);
+                double random_Mass_Radius = (2 + Math.random()) * 10; // All projectiles will have size between 20 and 30 pixels.
+                Projectile addedCircle = new Projectile(random_Mass_Radius, Math.random() * 5, Math.random() * 5, 50, 50, asteroidImage, random_Mass_Radius);
                 circles.add(addedCircle);
                 animationPane.getChildren().add(addedCircle.getCircle());
             }
